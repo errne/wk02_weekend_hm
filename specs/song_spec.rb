3e10b98b7c07dae42024657a28499331689a284d
Ernest Fraiman <upapaus@gmail.com>
2018-10-26 11:30:08 +0100

    song class created, title-test passed

--- specs/song_spec.rb
+++ specs/song_spec.rb
@@ -4,5 +4,9 @@ require_relative('../song')
 
 class Songtest < Minitest::Test
 
+  def test_song_has_name
+    song1 = Song.new("Mala vida")
+    assert_equal("Mala vida", song1.title)
+  end
 
 end
